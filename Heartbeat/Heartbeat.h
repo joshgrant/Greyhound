d5bd19d435593f5f10245327047944258b00b9de
Joshua Grant <joshgrant@pm.me>
2022-09-03 15:21:41 -0600

    Initial Commit

---
+++ Heartbeat/Heartbeat.h
@@ -0,0 +1,18 @@
+//
+//  Heartbeat.h
+//  Heartbeat
+//
+//  Created by Joshua Grant on 9/3/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Heartbeat.
+FOUNDATION_EXPORT double HeartbeatVersionNumber;
+
+//! Project version string for Heartbeat.
+FOUNDATION_EXPORT const unsigned char HeartbeatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Heartbeat/PublicHeader.h>
+
+
