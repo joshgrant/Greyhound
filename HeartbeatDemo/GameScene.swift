cd638f3f3d3a90ae19c15cf9d86a092c48d33090
Joshua Grant <joshgrant@pm.me>
2022-09-18 22:25:06 -0600

    Creating system subclasses

--- HeartbeatDemo/GameScene.swift
+++ HeartbeatDemo/GameScene.swift
@@ -9,6 +9,83 @@ import SpriteKit
 import GameplayKit
 import Heartbeat
 
+class CircleSystem: System
+{
+    // MARK: - Variables
+    
+    var xIdeal: () -> Double
+    var yIdeal: () -> Double = { 300 }
+    
+    var x: Stock
+    var y: Stock
+    
+    var xPos: Flow
+    var yPos: Flow
+    var xNeg: Flow
+    var yNeg: Flow
+    
+    var position: CGPoint
+    {
+        CGPoint(x: x.current, y: y.current)
+    }
+    
+    // MARK: - Initialization
+    
+    init(xIdeal: @escaping () -> Double)
+    {
+        self.xIdeal = xIdeal
+        
+        x = Stock(name: "x", current: 0, ideal: xIdeal, min: 0, max: 1000)
+        y = Stock(name: "y", current: 0, ideal: yIdeal, min: 0, max: 1000)
+        
+        xPos = Flow(name: "xPos", from: .source, to: x, amount: 1, duration: 0.01)
+        yPos = Flow(name: "xPos", from: .source, to: y, amount: 1, duration: 0.01)
+        xNeg = Flow(name: "xNeg", from: x, to: .sink, amount: 1, duration: 0.01)
+        yNeg = Flow(name: "yNeg", from: y, to: .sink, amount: 1, duration: 0.01)
+        
+        super.init(stocks: [x, y], flows: [xPos, yPos, xNeg, yNeg])
+    }
+}
+
+class SquareSystem: System
+{
+    // MARK: - Variables
+    
+    var x: Stock
+    var y: Stock
+    
+    var xPos: Flow
+    var yPos: Flow
+    var xNeg: Flow
+    var yNeg: Flow
+    
+    var position: CGPoint
+    {
+        CGPoint(x: x.current, y: y.current)
+    }
+    
+    func set(position: CGPoint)
+    {
+        x.current = position.x
+        y.current = position.y
+    }
+    
+    // MARK: - Initialization
+    
+    init()
+    {
+        x = Stock(name: "x", current: 1000, ideal: { 500 }, min: 600, max: 1000)
+        y = Stock(name: "y", current: 1000, ideal: { 500 }, min: 0, max: 1000)
+        
+        xPos = Flow(name: "xPos", from: .source, to: x, amount: 1, duration: 0.01)
+        yPos = Flow(name: "xPos", from: .source, to: y, amount: 1, duration: 0.01)
+        xNeg = Flow(name: "xNeg", from: x, to: .sink, amount: 1, duration: 0.01)
+        yNeg = Flow(name: "yNeg", from: y, to: .sink, amount: 1, duration: 0.01)
+        
+        super.init(stocks: [x, y], flows: [xPos, yPos, xNeg, yNeg])
+    }
+}
+
 class GameScene: SKScene
 {
     private var circle: SKShapeNode?
@@ -16,41 +93,11 @@ class GameScene: SKScene
     
     func goalX() -> Double
     {
-        squareSystem.stocks["x"]?.current ?? 0
+        squareSystem.position.x
     }
     
-    func goalY() -> Double
-    {
-        return 300
-    }
-    
-    private lazy var circleSystem: System =
-    {
-        let x = Stock(name: "x", current: 0, ideal: goalX, min: 0, max: 1000)
-        let y = Stock(name: "y", current: 0, ideal: goalY, min: 0, max: 1000)
-        
-        let moveX = Flow(name: "moveX", from: .source, to: x, amount: 1, duration: 0.01)
-        let moveY = Flow(name: "moveY", from: .source, to: y, amount: 1, duration: 0.01)
-        
-        let moveXNeg = Flow(name: "moveXNeg", from: x, to: .sink, amount: 1, duration: 0.01)
-        let moveYNeg = Flow(name: "moveYNeg", from: y, to:.sink, amount: 1, duration: 0.01)
-        
-        return System(stocks: [x, y], flows: [moveX, moveY, moveXNeg, moveYNeg])
-    }()
-    
-    private lazy var squareSystem: System =
-    {
-        let x = Stock(name: "x", current: 1000, ideal: { 500 }, min: 600, max: 1000)
-        let y = Stock(name: "y", current: 1000, ideal: { 500 }, min: 0, max: 1000)
-        
-        let moveX = Flow(name: "moveX", from: .source, to: x, amount: 1, duration: 0.01)
-        let moveY = Flow(name: "moveY", from: .source, to: y, amount: 1, duration: 0.01)
-        
-        let moveXNeg = Flow(name: "moveXNeg", from: x, to: .sink, amount: 1, duration: 0.01)
-        let moveYNeg = Flow(name: "moveYNeg", from: y, to:.sink, amount: 1, duration: 0.01)
-        
-        return System(stocks: [x, y], flows: [moveX, moveY, moveXNeg, moveYNeg])
-    }()
+    lazy var circleSystem = CircleSystem(xIdeal: goalX)
+    lazy var squareSystem = SquareSystem()
     
     override func didMove(to view: SKView)
     {
@@ -70,18 +117,14 @@ class GameScene: SKScene
     {
         AppDelegate.world.update(currentTime)
 
-        circle?.position.x = CGFloat(circleSystem.stocks["x"]?.current ?? 0)
-        circle?.position.y = CGFloat(circleSystem.stocks["y"]?.current ?? 0)
-        
-        square?.position.x = CGFloat(squareSystem.stocks["x"]?.current ?? 0)
-        square?.position.y = CGFloat(squareSystem.stocks["y"]?.current ?? 0)
+        circle?.position = circleSystem.position
+        square?.position = squareSystem.position
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
     {
         guard let position = touches.first?.location(in: self) else { return }
         
-        squareSystem.stocks["x"]?.current = position.x
-        squareSystem.stocks["y"]?.current = position.y
+        squareSystem.set(position: position)
     }
 }
