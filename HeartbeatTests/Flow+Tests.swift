f38d693f4047387cbfef844ef32f272106bff5a5
Joshua Grant <joshgrant@pm.me>
2022-09-25 10:27:23 -0600

    Writing test 1

--- HeartbeatTests/Flow+Tests.swift
+++ HeartbeatTests/Flow+Tests.swift
@@ -12,7 +12,46 @@ final class Flow_Tests: XCTestCase
 {
     func test_update_stockA_lowPressure()
     {
+        let stockA = Stock(
+            current: Measurement(value: 0, unit: .meters),
+            maximum: Measurement(value: 10, unit: .meters),
+            ideal: Measurement(value: 10, unit: .meters))
+        let stockB = Stock(
+            current: Measurement(value: 10, unit: .meters),
+            maximum: Measurement(value: 10, unit: .meters),
+            ideal: Measurement(value: 0, unit: .meters))
         
+        let flow = Flow(
+            limit: .init(value: 3, unit: .meters),
+            stockA: stockA,
+            stockB: stockB)
+        
+        flow.update(0)
+        flow.update(1)
+        
+        XCTAssertEqual(stockA.current.value, 3)
+        XCTAssertEqual(stockB.current.value, 7)
+        
+        flow.update(2)
+        
+        XCTAssertEqual(stockA.current.value, 5)
+        XCTAssertEqual(stockA.current.value, 4)
+        
+        flow.update(3)
+        
+        XCTAssertEqual(stockA.current.value, 9)
+        XCTAssertEqual(stockA.current.value, 1)
+        
+        flow.update(4)
+        
+        XCTAssertEqual(stockA.current.value, 10)
+        XCTAssertEqual(stockA.current.value, 0)
+        
+        flow.update(5)
+        
+        XCTAssertEqual(stockA.current.value, 10)
+        XCTAssertEqual(stockA.current.value, 0)
+
     }
     
     func test_update_stockA_highPressure()
@@ -21,6 +60,16 @@ final class Flow_Tests: XCTestCase
     }
     
     func test_update_stockA_equalPressure()
+    {
+        
+    }
+    
+    func test_update_stockA_noMaximum()
+    {
+        
+    }
+    
+    func test_update_stockB_noMaximum()
     {
         
     }
